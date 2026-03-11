69ed723e9cdb4c7a19555fad6b94a1306e509745
JuanRgz <juann.rogz27@gmail.com>
2026-03-10 19:47:42 -0600

    Proyecto listo para subir a Render

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// El puerto lo asigna Render, o usa el 3000 si es local
+const PORT = process.env.PORT || 3000;
+
+// IMPORTANTE: Servimos todos tus archivos (img, planetas, style.css)
+app.use(express.static(__dirname));
+
+// Ruta principal para abrir tu página
+app.get('/', (req, res) => {
+    // Usamos 'Index.html' porque así se llama tu archivo en la captura
+    res.sendFile(path.join(__dirname, 'Index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Tu sistema solar está listo en: http://localhost:${PORT}`);
+});
